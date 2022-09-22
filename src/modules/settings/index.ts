d3290c1182f118b975f0e57a4b5627000b6384bc
Crobat4 <104547700+Crobat4@users.noreply.github.com>
2022-09-22 08:03:47 -0300

    Changed berries setting text

--- src/modules/settings/index.ts
+++ src/modules/settings/index.ts
@@ -143,7 +143,7 @@ Settings.add(new Setting<string>('saveReminder', 'Save reminder interval (in gam
     (12 * HOUR).toString()));
 Settings.add(new Setting('breedingQueueSizeSetting', 'Breeding Queue Size', [], '-1'));
 // Crobat Fork
-Settings.add(new BooleanSetting('enableRemovedBerries', 'Enable removed berries from Official v0.9.4', false));
+Settings.add(new BooleanSetting('enableRemovedBerries', 'Enable berries removed from dungeons loot in Official v0.9.4 update', false));
 Settings.add(new BooleanSetting('confirmFightBoss', 'Confirm before fighting bosses in dungeons', false));
 
 // Sound settings

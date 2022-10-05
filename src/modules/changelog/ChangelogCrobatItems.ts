e2d99f60f22dc55d0cb6ba4fcb94373fa03ed9ae
Crobat4 <104547700+Crobat4@users.noreply.github.com>
2022-10-04 23:48:39 -0300

    Update ChangelogCrobatItems.ts

--- src/modules/changelog/ChangelogCrobatItems.ts
+++ src/modules/changelog/ChangelogCrobatItems.ts
@@ -13,6 +13,11 @@ const ChangelogCrobatItems = [
     new Changelog(changelogType.DEFAULT, '<div class="w-100 d-flex justify-content-around"><a href="https://github.com/Crobat4/pokeclicker" target="_blank">Repository</a><a href="https://github.com/Crobat4/Pokeclicker-desktop" target="_blank">Desktop build</a><a href="https://github.com/Crobat4/pokeclicker/wiki" target="_blank">Github Wiki</a></div>'),
     new Changelog(changelogType.DEFAULT, 'This will keep being updated as long as the thread is alive...'),
     // ⥂⥄
+    // v0.3.4
+    new ChangelogUpdate('v0.3.4', new Date(2022, 9 - 1, 21)),
+    new Changelog(changelogType.NEW, 'Added draggable container that shows the gem list with their amount'),
+    new Changelog(changelogType.CHANGE, 'Misc tooltips and settings text changes'),
+    new Changelog(changelogType.FIXED, 'Pinkan Butterfree and Ash\'s Butterfree gender types'),
     // v0.3.3
     new ChangelogUpdate('v0.3.3', new Date(2022, 9 - 1, 21)),
     new Changelog(changelogType.NEW, 'Added setting to enable removed berries in dungeons'),

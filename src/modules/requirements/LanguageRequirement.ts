56ea11a135fcb22f935cf16277ee961f0cb947af
osherwott <114451054+osherwott@users.noreply.github.com>
2022-12-02 16:33:49 -0600

    Create LanguageRequirement.ts

---
+++ src/modules/requirements/LanguageRequirement.ts
@@ -0,0 +1,20 @@
+import { Language } from '../translation/Language';
+import { AchievementOption } from '../GameConstants';
+import Requirement from './Requirement';
+
+export default class LanguageRequirement extends Requirement {
+    constructor(
+        private language: Language[],
+        option = AchievementOption.equal,
+    ) {
+        super(1, option);
+    }
+
+    public getProgress() {
+        return Number(App.translation.language === this.language);
+    }
+
+    public hint(): string {
+        return 'The language needs to be in ${(Language[language]).join(' or ')}';
+    }
+}
